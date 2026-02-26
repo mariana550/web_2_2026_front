b5c3191f02582c6cd72b56b021f9dcd34b727ca2
Mariana <villegasmariana398@gmail.com>
2026-02-25 21:18:46 -0500

    clase

--- ejemplos-web-2--standalone/src/app/app.routes.ts
+++ ejemplos-web-2--standalone/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './view/user/user.component/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'usuarios', component:UserComponent},
+    {path: '', redirectTo:'usuarios', pathMatch:'full'}
+];
---
+++ ejemplos-web-2--standalone/src/app/view/user/user.component/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user.component',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+}
