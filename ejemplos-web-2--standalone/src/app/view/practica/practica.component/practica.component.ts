795e8a9afe87e5bb326dfda8f4ba8de47260dc9c
Mariana <villegasmariana398@gmail.com>
2026-03-04 23:01:53 -0500

    clase componentes

---
+++ ejemplos-web-2--standalone/src/app/view/practica/practica.component/practica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practica.component',
+  imports: [],
+  templateUrl: './practica.component.html',
+  styleUrl: './practica.component.css',
+})
+export class PracticaComponent {
+
+}
--- ejemplos-web-2--standalone/src/app/view/user/user.component/user.component.ts
+++ ejemplos-web-2--standalone/src/app/view/user/user.component/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-user.component',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+
+  user: any[] = [];
+  exampleApi: any = [];
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef){
+    this.getUsers();
+    this.getExampleApi();
+  }
+  getUsers(){
+
+     this.http.get<any[]>('users.json').subscribe(data => {
+      this.user = data;
+      /* console.log(this.user); */    
+      this.cdr.detectChanges();  
+    }); 
+  }
+  
+  getExampleApi(){
+    this.http.get<any>('https://randomuser.me/api/').subscribe(data => {
+     this.exampleApi = data.results;
+     console.log(this.exampleApi);    
+     this.cdr.detectChanges();  
+   }); 
+
+  }
 }
